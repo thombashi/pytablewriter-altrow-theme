f7919c8d67b6258ae1e5d083fd66b99f42853b17
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2021-10-31 19:46:28 +0900

    Update an example

--- examples/set_theme.py
+++ examples/set_theme.py
@@ -1,15 +1,25 @@
 #!/usr/bin/env python3
 
-from pytablewriter import TableWriterFactory
+import pytablewriter as ptw
 
 
-def main():
-    writer = TableWriterFactory.create_from_format_name("markdown")
-    writer.headers = ["INT", "STR"]
-    writer.value_matrix = [[1, "hoge"], [2, "foo"], [3, "bar"]]
-    writer.margin = 1
+def main() -> None:
+    writer = ptw.TableWriterFactory.create_from_format_name(
+        "markdown",
+        table_name="altrow theme example",
+        headers=["INT", "STR"],
+        value_matrix=[
+            [1, "hoge"],
+            [2, "foo"],
+            [3, "bar"],
+        ],
+        margin=1,
+        theme="altrow",
+    )
+    writer.write_table()
+
+    print()
     writer.set_theme("altrow", color="yellow")
-
     writer.write_table()
 
 
