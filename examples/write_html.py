01c2a0845a24e82002686f2bacd4d1fadd15ce26
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2023-10-08 20:47:03 +0900

    Update examples

--- examples/write_html.py
+++ examples/write_html.py
@@ -8,7 +8,11 @@ from tblfaker import TableFaker
 
 def main() -> None:
     faker = TableFaker(seed=1)
-    data = faker.generate(["name", "address", "md5"], rows=8)
+    data = faker.generate(
+        ["name", "email", "address"],
+        headers=["Name", "Email", "Address"],
+        rows=8,
+    )
 
     writer = ptw.TableWriterFactory.create_from_format_name(
         "html",
@@ -17,9 +21,13 @@ def main() -> None:
     )
     writer.from_tabledata(data, is_overwrite_table_name=False)
     writer.set_theme("altrow")
-    with open(Path(__file__).parent.parent.joinpath("docs", "example.html"), "w") as f:
+
+    output_path = Path(__file__).parent.parent.joinpath("docs", "example.html")
+    with open(output_path, "w") as f:
         writer.stream = f
         writer.write_table(write_css=True)
+
+    print(f"generated: {output_path}")
 
 
 if __name__ == "__main__":
